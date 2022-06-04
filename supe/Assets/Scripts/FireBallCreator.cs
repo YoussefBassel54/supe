f15e53a7cafb3f4eda1df7317f8a09d20198cfd9
YoussefBassel54 <youssefbassel54@gmail.com>
2022-06-04 15:21:32 +0200

    basic controls and mechanics

---
+++ supe/Assets/Scripts/FireBallCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallCreator : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject rightOffset;
+    public GameObject leftOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateBall(ball);
+    }
+
+    void CreateBall(GameObject ball)
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) >= 0.1)
+        {
+            Instantiate(ball, leftOffset.transform.position, leftOffset.transform.rotation);
+        }
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) >= 0.1)
+        {
+            Instantiate(ball, rightOffset.transform.position, rightOffset.transform.rotation);
+        }
+
+    }
+
+    
+}
---
+++ supe/Assets/Scripts/HoverInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverInteraction : MonoBehaviour
+{
+
+    bool rotating = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            transform.Rotate(Vector3.up * 20);
+        }
+    }
+
+    public void HoverInteract(bool b)
+    {
+        rotating = b;
+    }
+}
